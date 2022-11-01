7e599545f6b4141d8bf5b04c0207503dfa8d0184
Lachezar.Stamenov <lachezar.stamenov@abv.bg>
2022-11-01 16:09:24 +0200

    Products by categories usl added.

--- e_fish_shop_app/store/urls.py
+++ e_fish_shop_app/store/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from e_fish_shop_app.store.views import StoreView
+from e_fish_shop_app.store.views import store
+
+# from e_fish_shop_app.store.views import store
 
 urlpatterns = (
-    path('', StoreView.as_view(), name='store'),
+    path('', store, name='store'),
+    path('<slug:category_slug>/', store, name='products by category'),
 )
--- e_fish_shop_app/store/views.py
+++ e_fish_shop_app/store/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic as views
+
+from e_fish_shop_app.category.models import Category
 from e_fish_shop_app.store.models import Product
 
 
-class StoreView(views.TemplateView):
-    template_name = 'store/store.html'
+def store(request, category_slug=None):
+    categories = None
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=categories, is_available=True)
+        products_count = len(Product.objects.all().filter(is_available=True))
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        products_count = len(Product.objects.all().filter(is_available=True))
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['products'] = Product.objects.all().filter(is_available=True)
-        context['products_count'] = len(Product.objects.all().filter(is_available=True))
-        return context
+    context = {
+        'products': products,
+        'products_count': products_count,
+    }
+    return render(request, 'store/store.html', context)
