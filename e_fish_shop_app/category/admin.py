52d49f98d63c2d1697dda0d7a3dbe31f72fb8cb6
Lachezar.Stamenov <lachezar.stamenov@abv.bg>
2022-11-01 09:28:54 +0200

    Category model added.

--- e_fish_shop_app/category/admin.py
+++ e_fish_shop_app/category/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from e_fish_shop_app.category.models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
