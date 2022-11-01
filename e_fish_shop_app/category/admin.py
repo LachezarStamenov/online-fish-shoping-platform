da33eafeb532174bb6efc9984a45e609eae6bbcb
Lachezar.Stamenov <lachezar.stamenov@abv.bg>
2022-11-01 12:28:05 +0200

    Pre populating category slug.

--- e_fish_shop_app/category/admin.py
+++ e_fish_shop_app/category/admin.py
@@ -5,5 +5,6 @@ from e_fish_shop_app.category.models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    prepopulated_fields = {'slug': ('category_name',)}
+    list_display = ('category_name', 'slug')
 
